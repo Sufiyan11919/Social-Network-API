f474159b074a94f9e7bb8d12d890156ec973d84e
sufi <sufiyanshaikh11919@gmail.com>
2022-10-31 08:56:18 -0700

    done with the models

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
-const userRoutes = require('./user-routes');
-const thoughtRoutes = require('./thought-routes');
+const userRoutes = require('./user-router');
+const thoughtRoutes = require('./thought-router');
 
 router.use('/users', userRoutes);
 router.use('/thoughts', thoughtRoutes);
